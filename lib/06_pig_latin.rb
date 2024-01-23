0b2f90c47dc3571ea2d55b6d2d8c886d32005d95
Marcaraph <ra.marcarini@gmail.com>
2024-01-23 12:13:30 +0100

    Modif comments 06

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -12,7 +12,8 @@ def translate(sentence)
                 # (.*) = correspond à la partie restante du mot après avoir identifié un pattern
             match ? match[2] + match[1] + "ay" : word + "ay"
             # l'expression match ? match[2] + match[1] + "ay" : word + "ay" se traduit comme suivant :
-                # si le mot commence par une voyelle (/^[aeiou]/i) ou "qu" ou "sch" ou une consonne suivie par qu (comme dans square) ou plusieurs consonnes (comme dans three), alors le mot est réorganisé de manière à ce qu'il commence par une voyelle, suivi de l'ensemble des consonnes correspondant aux conditions définies, suivi de "ay"
+                # si le mot commence par "qu" ou "sch" ou une consonne suivie par qu (comme dans square) ou plusieurs consonnes (comme dans three), alors le mot est réorganisé de manière à ce qu'il commence par une voyelle, suivi de l'ensemble des consonnes correspondant aux conditions définies, suivi de "ay"
+                # sinon, on prend le mot initial (word) auquel on ajoute +"ay"
         end
     end
     return translated_words.join(" ")
